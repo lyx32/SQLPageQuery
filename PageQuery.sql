09fc375a076b1c4f925375f053b1797ee737c2e7
lyx32 <7733528+lyx32@users.noreply.github.com>
2025-08-27 14:15:11 +0800

    Update PageQuery.sql
    1.处理第三版发现的问题
    2.因为第三版限制比较多，考虑进行第四版优化

--- PageQuery.sql
+++ PageQuery.sql
@@ -8,14 +8,17 @@
 				2.@orderby不为空的情况下。@orderby参数不能存在xxx.的前缀。因为最终我会写入到临时表，临时表不存在xxx.的情况
 				3.如果你的SQL语句需要返回多个结果集，那么每个语句中间需要用;分割
 				4.不支持太长的sql
+				5.orderby参数不能包含xxx.
+				6.sql参数中不能出现不同表的相同列名
 
 	2025-08-25 	1.第三版优化。处理oderby不能为空得情况
+	2025-08-27	1.此版本还存在多个影响实际使用的问题。正在考虑进行第四版优化
 */
 create procedure [dbo].[PageQuery]       
 	@page   int=1,				--要显示的页码   
 	@size   int=20,				--每页的大小   
-	@orderby nvarchar(300),		-- 排序字段，可以为空。如不为空则不能包含order by ，以及xxx. 
-	@sql   nvarchar(4000)		--要执行的sql语句   
+	@orderby nvarchar(300),		-- 排序字段
+	@sql   nvarchar(4000)		--要执行的sql语句 
 as   
 
 if(1 = charindex('(',ltrim(@sql))) begin
@@ -30,7 +33,7 @@ end
 declare @execTime varchar(30)=SYSDATETIME()
 declare @itemTime varchar(30)
   
-declare @tableName varchar(100) = '#'+left(convert(varchar(99),newId()),8)
+declare @tableName varchar(8) = '#'+left(convert(varchar(99),newId()),8)
 
 
 
@@ -42,7 +45,7 @@ set @sql= replace(@sql,'&','[@]')
 create table #sql( idx int not null,sql nvarchar(4000) not null )
 
 
-SELECT IDENTITY(int,1,1) as idx ,convert(nvarchar(4000),B.val) as sql into #sql2
+SELECT IDENTITY(int,1,1) as idx ,convert(nvarchar(4000),B.val) as sql into #sql_tmp
 FROM (
 	(SELECT [value] = CONVERT(XML, '<v>' + REPLACE(@sql, ';', '</v><v>') + '</v>') ) A 
 OUTER APPLY
@@ -50,21 +53,22 @@ OUTER APPLY
 )
 
 insert into #sql
-select idx,sql from #sql2 order by idx
-	  
+select idx,sql from #sql_tmp order by idx
+
 
 delete #sql where len(ltrim(rtrim(sql)))=0
 declare @sqlCount int
 select @sqlCount=count(*) from #sql
-
 declare @sql_item varchar(4000)
 declare @idx int
  
-select top 1 @sql_item=sql,@idx=idx from #sql order by idx desc
+select top 1 @sql_item=lower(sql),@idx=idx from #sql order by idx desc
+
 
 
 declare @fromStartIndex int =0
 declare @whereStartIndex int =0
+declare @whereLen int =5
 declare @temp_sql varchar(4000)=@sql_item
 declare @lc int=-999
 declare @rc int=-998
@@ -73,25 +77,21 @@ declare @k_l int = charindex('(',@temp_sql)
 declare @k_f int = charindex('from',@temp_sql)
 declare @k_r int = charindex(')',@temp_sql)
 declare @k_w int = charindex('where',@temp_sql)
-
-
+declare @sql_odrby_by int = charindex(' order by ',right(@temp_sql,100))
 
 set @itemTime=SYSDATETIME()
-
-if( @k_f > @k_l and @k_l > 0 ) begin	
-	while(( @lc<>@rc or @k_l < @k_f or @k_r < @k_f) and (@k_l>0 or @k_r>0)) begin
+ if( @k_f > @k_l and @k_l > 0 ) begin	
+	 while(( @lc<>@rc or @k_l < @k_f or @k_r < @k_f) and (@k_l>0 or @k_r>0)) begin
 		if(@lc=-999) begin set @lc=0 end
 		if(@rc=-998) begin set @rc=0 end
 		set @temp = substring( @temp_sql,0,@k_r+1)
 		set @lc = @lc+(SELECT count(*)-1 FROM ( (SELECT [value] = CONVERT(XML, '<v>' + REPLACE(@temp, '(', '</v><v>') + '</v>') ) A  OUTER APPLY (SELECT val = N.v.value('.', 'varchar(4000)') FROM A.[value].nodes('/v') N(v) ) B ))
 		set @rc = @rc+(SELECT count(*)-1 FROM ( (SELECT [value] = CONVERT(XML, '<v>' + REPLACE(@temp, ')', '</v><v>') + '</v>') ) A  OUTER APPLY (SELECT val = N.v.value('.', 'varchar(4000)') FROM A.[value].nodes('/v') N(v) ) B ))
-
 		set @temp_sql= substring( @temp_sql,@k_r+1,  4000)	
 		set @k_l = charindex('(',@temp_sql)
 		set @k_f = charindex('from',@temp_sql)
 		set @k_r = charindex(')',@temp_sql)
-	end
-	 
+	 end	 
 	set @fromStartIndex = charindex('from' ,@temp_sql)-1 + len(@sql_item) - len(@temp_sql);
  end else begin
 	set @fromStartIndex = charindex('from' ,@temp_sql)-1;
@@ -102,12 +102,11 @@ set @k_r = charindex(')',@temp_sql)
 set @k_w = charindex('where',@temp_sql)
 set @lc=-999
 set @rc=-998
- if( @k_w > @k_l and @k_l > 0 ) begin	 
+
+if( @k_w > @k_l and @k_l > 0 ) begin	
 	set @k_l = charindex('(',@temp_sql)
 	set @k_w = charindex('where',@temp_sql)
 	set @k_r = charindex(')',@temp_sql)
-
-
 	while(( @lc<>@rc or @k_l < @k_w or @k_r < @k_w) and (@k_l>0 or @k_r>0)) begin
 		if(@lc=-999) begin set @lc=0 end
 		if(@rc=-998) begin set @rc=0 end
@@ -115,39 +114,43 @@ set @rc=-998
 		set @temp = substring( @temp_sql,0,@k_r+1)
 		set @lc = @lc+(SELECT count(*)-1 FROM ( (SELECT [value] = CONVERT(XML, '<v>' + REPLACE(@temp, '(', '</v><v>') + '</v>') ) A  OUTER APPLY (SELECT val = N.v.value('.', 'varchar(4000)') FROM A.[value].nodes('/v') N(v) ) B ))
 		set @rc = @rc+(SELECT count(*)-1 FROM ( (SELECT [value] = CONVERT(XML, '<v>' + REPLACE(@temp, ')', '</v><v>') + '</v>') ) A  OUTER APPLY (SELECT val = N.v.value('.', 'varchar(4000)') FROM A.[value].nodes('/v') N(v) ) B ))
-		
 		set @temp_sql= substring( @temp_sql,@k_r+1,  4000)	
 		set @k_l = charindex('(',@temp_sql)
 		set @k_w = charindex('where',@temp_sql)
 		set @k_r = charindex(')',@temp_sql)
-	 end
-	 
+	end
 	set @whereStartIndex = charindex('where' ,@temp_sql)-1 + len(@sql_item) - len(@temp_sql);
  end else begin
-	if(len(@temp_sql) = len(@sql_item)) begin
-		set @whereStartIndex = charindex('where' ,@temp_sql) -1;
+	set @whereLen = 0
+	set @whereStartIndex = charindex('where',@sql_item)-1
+	if(@whereStartIndex = -1) begin
+		set @whereStartIndex = charindex('group by',@sql_item)-1	
 	end else begin
-		set @whereStartIndex = @fromStartIndex + charindex('where' ,@temp_sql) - 2;
+		set @whereLen = 5;	
+	end
+	if(@whereStartIndex = -1) begin
+		set @whereStartIndex = charindex('order by',@sql_item)-1	
+	end
+	if(@whereStartIndex = -1) begin
+		set @whereStartIndex = len(@sql_item)	
 	end
  end
 
  
+
 DECLARE @ErrorMessage NVARCHAR(4000);
 DECLARE @ErrorSeverity INT;
 DECLARE @ErrorState INT;
 
-print 'from='+convert(varchar(20),@fromStartIndex)+'  where='+convert(varchar(20),@whereStartIndex)
 if(@fromStartIndex >-1) begin
-	if(@whereStartIndex = -1) begin
-		set @whereStartIndex = len(@sql_item)
-	end
 	declare @rawSQLFrom nvarchar(4000)=substring( @sql_item,@fromStartIndex  ,4000)
 	set @temp_sql = substring( @sql_item,0,@fromStartIndex )+' into '+@tableName+' '+substring( @sql_item,@fromStartIndex +1 ,@whereStartIndex - @fromStartIndex)+' where 1=2 ';
-	if(len(substring( @sql_item,@whereStartIndex+6  ,4000))>0) begin
-		set @temp_sql = @temp_sql+' and '+substring( @sql_item,@whereStartIndex+6  ,4000)
+	if(@whereLen>0) begin
+		set @temp_sql = @temp_sql+' and '
 	end
+	set @temp_sql=@temp_sql+substring( @sql_item,@whereStartIndex+@whereLen+1  ,4000)
 	declare @isReplace varchar(1)='n'
-
+	
 	if(ISNULL(@orderby,'')='') begin
 		set @itemTime=SYSDATETIME()
 		declare @nvarchar nvarchar(max)=replace(replace(replace(@temp_sql,'[lt]','<'),'[rt]','>'),'[@]','&') +N';select @primaryKeyName=a.name from tempdb.sys.columns a inner join tempdb.sys.objects b on a.[object_id]=b.[object_id] and b.type=''u'' where b.name LIKE '''+@tableName+'%'' and a.is_identity=''1'''		
@@ -159,11 +162,15 @@ if(@fromStartIndex >-1) begin
 			RAISERROR (@ErrorMessage,@ErrorSeverity, @ErrorState );
 			return ;
 		end catch
-
+		
 		if(len(@orderby)=0) begin 
 			set @orderby='N_7777'  
 			set @isReplace='y'
 			set @sql_item = substring( @sql_item,0,@fromStartIndex )+',IDENTITY(int,1,1) as N_7777 into '+@tableName+' '+substring( @sql_item,@fromStartIndex  ,4000)
+		end else begin
+			if(@sql_odrby_by>0) begin
+				THROW 777777,'该sql语法无法自动解析，请传入存储过程的@orderby参数',1
+			end
 		end
 	end
 	if('n' = @isReplace) begin
@@ -178,7 +185,6 @@ if(@fromStartIndex >-1) begin
  declare @nsql nvarchar(max)
  set @nsql=(SELECT  ';'+sql FROM #sql order by idx FOR XML PATH(''))
  set @nsql = replace(replace(replace(@nsql,'[lt]','<'),'[rt]','>'),'[@]','&')
- print @nsql
 
 begin try
 	exec sp_executesql @nsql
